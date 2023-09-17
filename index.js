db95d06c0c8db64e00bb6212c258babbb9dd56d9
Connor Adams <connorvadams@gmail.com>
2023-09-17 11:49:53 -0600

    Submitting lab.

--- index.js
+++ index.js
@@ -1,19 +1,7 @@
 const formData = {
-    name: 'Thor Odinson',
-    email: 'godofthunder@gmail.com',
+    name: 'Steve',
+    email: 'steve@steve.com',
 };
-
-// const newValue = {
-//     email: 'newemail@gmail.com'
-// };
-
-// const destroyConfig = {
-//     method: 'DELETE',
-//     headers: {
-//         'Content-Type': 'application/json',
-//         'Accept': 'application/json',
-//     },
-// };
 
 const createConfig = { //Generalizes a template to be used every time submitData is called, so that function is more concise and readable.
     method:'POST',
@@ -24,17 +12,8 @@ const createConfig = { //Generalizes a template to be used every time submitData
     body: JSON.stringify(formData),
 };
 
-// const updateConfig = {
-//     method: 'PATCH',
-//     headers: {
-//         'Content-Type': 'application/json',
-//         'Accept': 'application/json',
-//     },
-//     body: JSON.stringify(newValue)
-// };
-
 const submitData = () => {
-    fetch('http://localhost:3000/users', createConfig)
+    return fetch('http://localhost:3000/users', createConfig)
     .then(resp => resp.json())
     .then(data => {
         document.querySelector('body').append(data.id)
@@ -42,19 +21,6 @@ const submitData = () => {
 
     .catch(error => {
         alert('An error has occured')
-        console.log('An error has occured.')
-    })
+        document.querySelector('body').append('Unauthorized Access.')
+    });
 };
-
-// const deleteUser = (id) => {
-//     fetch(`http://localhost:3000/users/${id}`, destroyConfig)
-//         console.log('Resource deleted successfully.')
-// };
-
-// const updateEmail = (id, newValue) => {
-
-//     fetch(`http://localhost:3000/users/${id}`, updateConfig)
-//         .then(resp => resp.json())
-//         .then(data => data.email = newValue.email)    
-//     console.log('Resource updated successfully')
-// }
